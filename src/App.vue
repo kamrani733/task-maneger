92459b3db81ebaa63f4f4151389416d0ffdde6d6
fateme <fatemekamrani733@gmail.com>
2024-12-28 18:08:29 +0330

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,16 @@
 <template>
-  <div class="font-sans p-6">
-    <h1 class="text-2xl font-bold mb-4">مدیریت وظایف</h1>
-    <div class="flex flex-wrap gap-6">
-       <TaskCard>
-        <template #title>
-          <h3 class="text-lg font-semibold">وظیفه ۱: مطالعه Vue.js</h3>
-        </template>
-        <template #actions>
-          <button
-            @click="markAsDone('مطالعه Vue.js')"
-            class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition"
-          >
-            انجام شد
-          </button>
-          <button
-            @click="deleteTask('مطالعه Vue.js')"
-            class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-          >
-            حذف
-          </button>
-        </template>
-        <p class="text-sm text-gray-700">
-          یادگیری اسلات‌ها و کامپوننت‌ها در Vue.js
-        </p>
-      </TaskCard>
-
-       <TaskCard>
-        <template #title>
-          <h3 class="text-lg font-semibold">وظیفه ۲: تمرین برنامه‌نویسی</h3>
-        </template>
-        <template #actions>
-          <button
-            @click="markAsDone('تمرین برنامه‌نویسی')"
-            class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition"
-          >
-            انجام شد
-          </button>
-          <button
-            @click="deleteTask('تمرین برنامه‌نویسی')"
-            class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-          >
-            حذف
-          </button>
-        </template>
-        <p class="text-sm text-gray-700">
-          حل چند سوال الگوریتمی از سایت LeetCode
-        </p>
-      </TaskCard>
+  <div class="bg-[#ECECE5] min-h-screen flex items-center justify-center">
+    <div class="w-[90%] max-w-[1800px] bg-white rounded-lg p-10">
+      <Sidebar />
+      <MainPage />
     </div>
   </div>
 </template>
 
+
 <script setup>
-import TaskCard from "./components/TaskCard.vue";
-
-function markAsDone(task) {
-  alert(`وظیفه "${task}" انجام شد!`);
-}
-
-function deleteTask(task) {
-  alert(`وظیفه "${task}" حذف شد!`);
-}
+import Sidebar from "./components/sideBar.vue";
+import MainPage from "./components/mainPage.vue";
 </script>
 
-<style>
- </style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: 'class', // or 'media' or 
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#93C5FD",
+          DEFAULT: "#3772FF",
+          dark: "#1E40AF",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
