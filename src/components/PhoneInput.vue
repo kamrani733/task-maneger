fc9d927e97b6187b597f3562e0cb20726b5a0883
fateme <fatemekamrani733@gmail.com>
2025-01-02 23:05:33 +0330

    add contry list

--- src/components/PhoneInput.vue
+++ src/components/PhoneInput.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
 import { defineEmits, defineProps, useAttrs } from "vue";
 import type { PhoneInputProps } from "./PhoneInput.type";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import countries from "./country.json";
 
-const countries = ref([
-  {
-    name: "United States",
-    code: "+1",
-    icon: "/icon/us.png",
-  },
-  {
-    name: "United Kingdom",
-    code: "+44",
-    icon: "/icon/uk.png",
-  },
-  {
-    name: "Australia",
-    code: "+61",
-    icon: "/icon/ar.png",
-  },
-  {
-    name: "Germany",
-    code: "+49",
-    icon: "/icon/gr.png",
-  },
-  {
-    name: "France",
-    code: "+33",
-    icon: "/icon/fr.png",
-  },
-]);
-
-const selectedCountry = ref(countries.value[0]);
+const selectedCountry = ref(countries[0]);
 const isDropdownOpen = ref(false);
 
 const props = defineProps<PhoneInputProps>();
@@ -43,21 +16,23 @@ const phoneNumber = ref(props.modelValue || "");
 const toggleDropdown = () => {
   isDropdownOpen.value = !isDropdownOpen.value;
 };
-import { computed } from "vue";
 
 const labelFor = computed(() => {
-  return (attrs.id as string) || (attrs.name as string) || "";
+  return (attrs.id as string) || (attrs.name as string) || "default-id";
 });
-
-const selectCountry = (country: (typeof countries.value)[number]) => {
+const searchQuery = ref("");
+const filteredCountries = computed(() => {
+  return countries.filter(country => {
+    return country.name.toLowerCase().includes(searchQuery.value.toLowerCase());
+  });
+});
+const selectCountry = (country: (typeof countries)[number]) => {
   selectedCountry.value = country;
   isDropdownOpen.value = false;
 };
-
-const updatePhoneNumber = (event: Event) => {
-  const target = event.target as HTMLInputElement;
-  phoneNumber.value = target.value;
-};
+const selectedCountryImg = computed(() => {
+  return `https://flagcdn.com/${selectedCountry.value.code.toLowerCase()}.svg`;
+});
 </script>
 
 <template>
@@ -66,24 +41,35 @@ const updatePhoneNumber = (event: Event) => {
       <div class="relative">
         <button
           @click="toggleDropdown"
-          class="h-12 p-4 bg-[#272e3c] text-white  rounded-l-2xl hover:bg-[#434e62]   flex items-center space-x-2"
+          class="h-12 p-4 bg-[#272e3c] text-white rounded-l-2xl hover:bg-[#434e62] flex items-center space-x-2"
         >
-          <img width="20" :src="selectedCountry.icon" alt="Country flag" />
-          <span>{{ selectedCountry.code }}</span>
+          <img width="20" :src="selectedCountryImg" alt="Country flag" />
+          <span>{{ selectedCountry.code }}<span class="pl-2">{{ selectedCountry.dialling_code }}</span></span>
         </button>
 
         <ul
           v-if="isDropdownOpen"
-          class="absolute top-full left-0 mt-1 w-64 bg-[#272e3c] text-white rounded shadow-lg z-10"
+          class="absolute top-full left-0 mt-1 max-h-[300px] overflow-y-scroll w-64 bg-[#272e3c] text-white rounded shadow-lg z-10"
         >
+          <input
+            v-model="searchQuery"
+            type="text"
+            placeholder="Search country"
+            class="w-full p-2 bg-[#434e62] text-white rounded"
+          />
           <li
-            v-for="country in countries"
+            v-for="country in filteredCountries"
             :key="country.code"
             @click="selectCountry(country)"
             class="h-12 p-4 hover:bg-[#434e62] cursor-pointer flex items-center space-x-2"
           >
-            <img width="20" :src="country.icon" alt="Country flag" />
-            <span class="text-sm">{{ country.name }} ({{ country.code }})</span>
+            <img
+              width="20"
+              :src="`https://flagcdn.com/${country.code.toLowerCase()}.svg`"
+              alt="Country flag"
+              loading="lazy"
+            />
+            <span class="text-sm">{{ country.name }} ({{ country.dialling_code }})</span>
           </li>
         </ul>
       </div>
@@ -92,12 +78,11 @@ const updatePhoneNumber = (event: Event) => {
         v-bind="attrs"
         :value="phoneNumber"
         :placeholder="(attrs.placeholder as string) || ''"
-        class="block w-full h-12 p-4   rounded-r-lg  bg-[#272e3c]  text-white   text-base focus:outline-none caret-primary"
+        class="block w-full h-12 p-4 rounded-r-lg bg-[#272e3c] text-white text-base focus:outline-none caret-primary"
         :class="{
           'border-2 border-negative focus:ring-negative focus:border-negative':
             attrs.errorMessage,
-          '   focus:ring-primary focus:border-primary':
-            !attrs.errorMessage,
+          '   focus:ring-primary focus:border-primary': !attrs.errorMessage,
         }"
       />
     </div>
@@ -119,3 +104,20 @@ const updatePhoneNumber = (event: Event) => {
     </p>
   </div>
 </template>
+<style scoped>
+::-webkit-scrollbar {
+  width: 4px;
+}
+
+::-webkit-scrollbar-track {
+  background: #353030;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #777373;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
